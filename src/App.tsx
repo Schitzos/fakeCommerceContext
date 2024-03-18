cc8c0075e7cb2492e00763ec9054e78a131b60d4
Schitzos <rizky.rachmawan25@gmail.com>
2024-03-19 00:53:42 +0700

    fix: remove performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import Navigation from './navigation';
 import ContextProvider from './context';
 import 'react-native-gesture-handler';
-import {
-  RenderPassReport,
-  PerformanceProfiler,
-} from '@shopify/react-native-performance';
 
 function App() {
-  const onReportPrepared = useCallback((report: RenderPassReport) => {
-    console.log(report);
-  }, []);
-
   return (
-    <PerformanceProfiler onReportPrepared={onReportPrepared}>
-      <ContextProvider>
-        <Navigation />
-      </ContextProvider>
-    </PerformanceProfiler>
+    <ContextProvider>
+      <Navigation />
+    </ContextProvider>
   );
 }
 
